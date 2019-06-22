a1cb08d00e846295855751dd0186dcc6171a2ce4
StasRomanov <stasromanov801@gmail.com>
2019-06-22 15:50:49 +0300

    js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@ var form = document.querySelector('form');
 var visuality = false;
 var checkIn = document.querySelector('.check-in-date-form');
 var departure = document.querySelector('.departure-date-form');
-var cnt1Value = 2;
-var cnt2Value = 1;
-var cnt1 = document.querySelector('.cnt1');
-var cnt2 = document.querySelector('cnt2');
-var minus1 = document.querySelector('.minus-container1');
-var plus1 = document.querySelector('.plus-container1');
-var plus2 = document.querySelector('.plus-container2');
-var minus2 = document.querySelector('.minus-container2');
 form.classList.add('search-hidden');
 seacrch.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -27,9 +19,13 @@ seacrch.addEventListener('click', function (evt) {
 });
 
 form.addEventListener('submit', function(evt){
-  if (checkIn.value === ''){
+  if (!checkIn.value || !departure.value){
     evt.preventDefault();
+  }
+  if (!checkIn.value) {
     checkIn.classList.add('warning');
+  }
+  if (!departure.value) {
     departure.classList.add('warning');
   }
 });
@@ -41,38 +37,4 @@ departure.addEventListener('click', function(){
   departure.classList.remove('warning');
 });
 
-minus1.addEventListener('click', function () {
-  if (cnt1Value < 0) {
-    cnt1Value = 0;
-  }
-  else {
-    cnt1.innerText = cnt1Value--;
-  }
-});
 
-plus1.addEventListener('click', function () {
-  if (cnt1Value > 9) {
-    cnt1Value = 9;
-  }
-  else {
-    cnt1.innerText = cnt1Value++;
-  }
-});
-
-minus2.addEventListener('click', function () {
-  if (cnt2Value < 0) {
-    cnt2Value = 0;
-  }
-  else {
-    cnt2.innerText = cnt2Value--;
-  }
-});
-
-plus2.addEventListener('click', function () {
-  if (cnt2Value > 9) {
-    cnt2Value = 9;
-  }
-  else {
-    cnt2.innerText = cnt2Value++;
-  }
-});
